4ceadf22fa2141ba44d33fcf9163fe13c14e89d1
yassa98 <yasirudeshan3@gmail.com>
2021-05-07 23:55:01 +0530

    BasePages

---
+++ app/src/main/java/com/example/mad_project/AddCard.java
@@ -0,0 +1,25 @@
+package com.example.vesloostore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.material.textfield.TextInputLayout;
+
+
+public class AddCard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+
+
+    }
+}
